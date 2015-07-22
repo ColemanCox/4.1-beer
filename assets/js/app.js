b31aa38041ae388490380964b107ec48e84c6509
Coleman Cox <colezane82@gmail.com>
2015-07-21 22:42:04 -0500

    not sure how to call selectors in handlebars

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,19 @@
+
 (function() {
   var beersEl = document.querySelector('.beers');
-   var templateString = document.querySelector('#beers-template').innerHTML;
+  var templateString = document.querySelector('#beers-template').innerHTML;
+  var h = Handlebars.compile(templateString);
 
-
-    var h = Handlebars.compile(templateString);
-
-    var result = beersEl.data.results.reduce(function(beer) {
-      return h(beers);
-     }, '');
+  var result = beerData.data.reduce(function(previous, beers) {
+    return previous + h(beers);
+  }, '');
 
   beersEl.innerHTML = result;
+
 })();
+var gridModeEl = document.querySelector('.grid-mode');
+
+gridModeEL.onClick = function() {
+  alert('hey sucka');
+
+};
